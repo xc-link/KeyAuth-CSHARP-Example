da3dc75e93448593f115be685b57c6cdad446dd2
xc-link <cai_liyong@qq.com>
2024-10-08 11:04:34 +0800

    Create KeyAuth1.cs

---
+++ KeyAuth1.cs
@@ -0,0 +1,7 @@
+public static api KeyAuthApp = new api(
+    name: "AJX", // Application Name
+    ownerid: "o0dHuOg0Bv", // Owner ID
+    secret: "f8f4af5e0759edfb5c948e2e1b3efaa36e23c5543f16a94288fc1785d3e4e3b1", // Application Secret
+    version: "1.0" // Application Version /*
+    //path: @"Your_Path_Here" // (OPTIONAL) see tutorial here https://www.youtube.com/watch?v=I9rxt821gMk&t=1s
+);
