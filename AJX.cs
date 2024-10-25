b4b6e76f9f90f123122295c576e2d125a8dd8a82
xc-link <cai_liyong@qq.com>
2024-10-26 01:45:22 +0800

    Update AJX.cs

--- AJX.cs
+++ AJX.cs
@@ -3,5 +3,5 @@ public static api KeyAuthApp = new api(
     ownerid: "o0dHuOg0Bv", // Owner ID
     secret: "f8f4af5e0759edfb5c948e2e1b3efaa36e23c5543f16a94288fc1785d3e4e3b1", // Application Secret
     version: "1.0" // Application Version /*
-    path:"d34296d837289aaea0b60b680e173b18"//@"Your_Path_Here" // (OPTIONAL) see tutorial here https://www.youtube.com/watch?v=I9rxt821gMk&t=
+    path:@"d34296d837289aaea0b60b680e173b18" // (OPTIONAL) see tutorial here https://www.youtube.com/watch?v=I9rxt821gMk&t=
 );
