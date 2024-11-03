fbe80266cf5a0ef619e9667feee472b7aef2b707
xc-link <cai_liyong@qq.com>
2024-11-04 02:34:27 +0800

    Update AJX.cs

--- AJX.cs
+++ AJX.cs
@@ -1,7 +1,7 @@
 public static api KeyAuthApp = new api(
     name: "AJX", // Application Name
     ownerid: "o0dHuOg0Bv", // Owner ID
-    secret: "f8f4af5e0759edfb5c948e2e1b3efaa36e23c5543f16a94288fc1785d3e4e3b1", // Application Secret
+    secret: "07e98db6aa912e152c9192953acd21dc938426e18c901470ab4da59495b201ef", // Application Secret
     version: "1.0" // Application Version /*
     path:@"d34296d837289aaea0b60b680e173b18" // (OPTIONAL) see tutorial here https://www.youtube.com/watch?v=I9rxt821gMk&t=
 );
